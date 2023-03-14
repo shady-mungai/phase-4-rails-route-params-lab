093103262f84e25fc9383342343a7f9180b8d43b
shady-mungai <shadrackmungai10@gmail.com>
2023-03-14 12:27:10 +0300

    Completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+    student = Student.find_by(first_name:params[:first_name])
+    render json: student
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
